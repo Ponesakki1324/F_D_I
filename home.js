a355afa7030401d808f66146a2e44450cf0515bd
unknown <ponnibs98@gmail.com>
2021-03-30 14:14:54 +0530

    commit

--- home.js
+++ home.js
@@ -1,108 +1,28 @@
-$(document).ready(function() {
-    $('#contact_form').bootstrapValidator({
-        // To use feedback icons, ensure that you use Bootstrap v3.1.0 or later
-        feedbackIcons: {
-            valid: 'glyphicon glyphicon-ok',
-            invalid: 'glyphicon glyphicon-remove',
-            validating: 'glyphicon glyphicon-refresh'
-        },
-        fields: {
-            first_name: {
-                validators: {
-                        stringLength: {
-                        min: 2,
-                    },
-                        notEmpty: {
-                        message: 'Please enter your First Name'
-                    }
-                }
-            },
-             last_name: {
-                validators: {
-                     stringLength: {
-                        min: 2,
-                    },
-                    notEmpty: {
-                        message: 'Please enter your Last Name'
-                    }
-                }
-            },
-			 user_name: {
-                validators: {
-                     stringLength: {
-                        min: 8,
-                    },
-                    notEmpty: {
-                        message: 'Please enter your Username'
-                    }
-                }
-            },
-			 user_password: {
-                validators: {
-                     stringLength: {
-                        min: 8,
-                    },
-                    notEmpty: {
-                        message: 'Please enter your Password'
-                    }
-                }
-            },
-			confirm_password: {
-                validators: {
-                     stringLength: {
-                        min: 8,
-                    },
-                    notEmpty: {
-                        message: 'Please confirm your Password'
-                    }
-                }
-            },
-            email: {
-                validators: {
-                    notEmpty: {
-                        message: 'Please enter your Email Address'
-                    },
-                    emailAddress: {
-                        message: 'Please enter a valid Email Address'
-                    }
-                }
-            },
-            contact_no: {
-                validators: {
-                  stringLength: {
-                        min: 12, 
-                        max: 12,
-                    notEmpty: {
-                        message: 'Please enter your Contact No.'
-                     }
-                }
-            },
-			 department: {
-                validators: {
-                    notEmpty: {
-                        message: 'Please select your Department/Office'
-                    }
-                }
-            },
-                }
-            }
-        })
-        .on('success.form.bv', function(e) {
-            $('#success_message').slideDown({ opacity: "show" }, "slow") // Do something ...
-                $('#contact_form').data('bootstrapValidator').resetForm();
+$(function(){
+    // This code is not even almost production ready. It's 2am here, and it's a cheap proof-of-concept if anything.
+    $(".img-modal-btn.right").on('click', function(e){
+        e.preventDefault();
+        cur = $(this).parent().find('img:visible()');
+        next = cur.next('img');
+        par = cur.parent();
+        if (!next.length) { next = $(cur.parent().find("img").get(0)) }
+        cur.addClass('hidden');
+        next.removeClass('hidden');
+        
+        return false;
+    })
+    
+    $(".img-modal-btn.left").on('click', function(e){
+        e.preventDefault();
+        cur = $(this).parent().find('img:visible()');
+        next = cur.prev('img');
+        par = cur.parent();
+        children = cur.parent().find("img");
+        if (!next.length) { next = $(children.get(children.length-1)) }
+        cur.addClass('hidden');
+        next.removeClass('hidden');
+        
+        return false;
+    })
 
-            // Prevent form submission
-            e.preventDefault();
-
-            // Get the form instance
-            var $form = $(e.target);
-
-            // Get the BootstrapValidator instance
-            var bv = $form.data('bootstrapValidator');
-
-            // Use Ajax to submit form data
-            $.post($form.attr('action'), $form.serialize(), function(result) {
-                console.log(result);
-            }, 'json');
-        });
 });
